e9b838c6d9dd1b2310e6c5b612199be7f3ef309f
Soufian Elhanafi <selhanaf@everis.com>
2021-02-02 17:05:12 +0100

    update hibernate version and sql queries

--- src/main/resources/sql/cars.sql
+++ src/main/resources/sql/cars.sql
@@ -1,14 +1,10 @@
-INSERT INTO public.car (id,created_date,updated_date,brand,country,registration) VALUES
-	 ('d29d6854-f47a-4a76-8754-2dd7906502ea','2021-02-01 15:27:53.795','2021-02-01 15:27:53.795','Brand Name','Country','2021-02-01 15:27:53.792'),
-	 ('95f8afff-57e3-4738-a3cf-f749960eb4c9','2021-02-01 15:45:57.244','2021-02-01 15:45:57.27','Brand9','Coountr9','2012-02-09 15:45:57.267'),
-	 ('73c74ca8-17ab-4f78-93c3-e6040efd4c1d','2021-02-01 15:48:07.574','2021-02-01 15:48:07.723','Brand8','Coountr8','2013-02-08 15:48:07.723'),
-	 ('d32e9559-14b4-4d01-aeaf-c948e4877792','2021-02-01 15:49:23.338','2021-02-01 15:49:23.338','Brand0','Coountr0','2021-01-31 15:49:23.335'),
-	 ('4b62d61e-6fa4-407f-bb27-c9384ce88bd7','2021-02-01 15:49:23.352','2021-02-01 15:49:23.352','Brand1','Coountr1','2020-02-01 15:49:23.352'),
-	 ('b2e60336-2e1f-436a-9dbc-a9b35aa65bd4','2021-02-01 15:49:23.353','2021-02-01 15:49:23.353','Brand2','Coountr2','2019-02-02 15:49:23.353'),
-	 ('abf85236-7315-45ab-acfe-c6eeaaafb3d3','2021-02-01 15:49:23.353','2021-02-01 15:49:23.353','Brand3','Coountr3','2018-02-03 15:49:23.353'),
-	 ('7edd0904-b439-4fa4-884b-c067fe08ae2e','2021-02-01 15:49:23.354','2021-02-01 15:49:23.354','Brand4','Coountr4','2017-02-04 15:49:23.354'),
-	 ('669007e1-91a7-48e5-852a-8a52306eed2e','2021-02-01 15:49:23.354','2021-02-01 15:49:23.354','Brand5','Coountr5','2016-02-05 15:49:23.354'),
-	 ('14a3036c-6c47-490e-87e0-eca768310abd','2021-02-01 15:49:23.354','2021-02-01 15:49:23.354','Brand6','Coountr6','2015-02-06 15:49:23.354');
-INSERT INTO public.car (id,created_date,updated_date,brand,country,registration) VALUES
-	 ('515002e9-4a1e-4f99-8736-5fb5101f89c2','2021-02-01 15:49:23.354','2021-02-01 15:49:23.354','Brand7','Coountr7','2014-02-07 15:49:23.354'),
-	 ('fe5aa7c5-db7c-4b02-94ba-747657df8bad','2021-02-01 15:49:23.355','2021-02-01 15:49:23.355','Brand8','Coountr8','2013-02-08 15:49:23.355');
+INSERT INTO car (id,created_date,updated_date,brand,country,registration) VALUES ('d29d6854-f47a-4a76-8754-2dd7906502ea','2021-02-01 15:27:53.795','2021-02-01 15:27:53.795','Brand Name','Country','2021-02-01 15:27:53.792');
+INSERT INTO car (id,created_date,updated_date,brand,country,registration) VALUES ('95f8afff-57e3-4738-a3cf-f749960eb4c9','2021-02-01 15:45:57.244','2021-02-01 15:45:57.27','Brand9','Coountr9','2012-02-09 15:45:57.267');
+INSERT INTO car (id,created_date,updated_date,brand,country,registration) VALUES ('73c74ca8-17ab-4f78-93c3-e6040efd4c1d','2021-02-01 15:48:07.574','2021-02-01 15:48:07.723','Brand8','Coountr8','2013-02-08 15:48:07.723');
+INSERT INTO car (id,created_date,updated_date,brand,country,registration) VALUES ('d32e9559-14b4-4d01-aeaf-c948e4877792','2021-02-01 15:49:23.338','2021-02-01 15:49:23.338','Brand0','Coountr0','2021-01-31 15:49:23.335');
+INSERT INTO car (id,created_date,updated_date,brand,country,registration) VALUES ('4b62d61e-6fa4-407f-bb27-c9384ce88bd7','2021-02-01 15:49:23.352','2021-02-01 15:49:23.352','Brand1','Coountr1','2020-02-01 15:49:23.352');
+INSERT INTO car (id,created_date,updated_date,brand,country,registration) VALUES ('b2e60336-2e1f-436a-9dbc-a9b35aa65bd4','2021-02-01 15:49:23.353','2021-02-01 15:49:23.353','Brand2','Coountr2','2019-02-02 15:49:23.353');
+INSERT INTO car (id,created_date,updated_date,brand,country,registration) VALUES ('abf85236-7315-45ab-acfe-c6eeaaafb3d3','2021-02-01 15:49:23.353','2021-02-01 15:49:23.353','Brand3','Coountr3','2018-02-03 15:49:23.353');
+INSERT INTO car (id,created_date,updated_date,brand,country,registration) VALUES ('7edd0904-b439-4fa4-884b-c067fe08ae2e','2021-02-01 15:49:23.354','2021-02-01 15:49:23.354','Brand4','Coountr4','2017-02-04 15:49:23.354');
+INSERT INTO car (id,created_date,updated_date,brand,country,registration) VALUES ('669007e1-91a7-48e5-852a-8a52306eed2e','2021-02-01 15:49:23.354','2021-02-01 15:49:23.354','Brand5','Coountr5','2016-02-05 15:49:23.354');
+INSERT INTO car (id,created_date,updated_date,brand,country,registration) VALUES ('14a3036c-6c47-490e-87e0-eca768310abd','2021-02-01 15:49:23.354','2021-02-01 15:49:23.354','Brand6','Coountr6','2015-02-06 15:49:23.354');
