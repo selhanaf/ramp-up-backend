1645030ac107518d0dcc205862856b0786de782e
Soufian Elhanafi <selhanaf@everis.com>
2021-02-08 13:13:00 +0100

    remove unused imports

--- src/main/java/com/car/app/service/CarService.java
+++ src/main/java/com/car/app/service/CarService.java
@@ -1,11 +1,6 @@
 package com.car.app.service;
 
-import java.util.Date;
 import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
